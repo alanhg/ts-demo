6c0ef275db14efd4e2656d4906a04e902199c5cb
alanhe <qianghe421@163.com>
2020-05-16 15:51:08 +0800

    add class decorator contains param

--- test.ts
+++ test.ts
@@ -1,4 +1,4 @@
-@modifyProps
+@modifyProps('2020')
 export default class Greeter {
     static greeting = '';
 
@@ -9,10 +9,12 @@ function modifyProp(target: any, propertyKey: string) {
     target[propertyKey] = Math.random().toString();
 }
 
-function modifyProps(constructor: any) {
-    Object.keys(constructor).forEach(
-        item => constructor[item] = item)
-    return constructor;
+function modifyProps(prefix: string) {
+    return (constructor: any) => {
+        Object.keys(constructor).forEach(
+            item => constructor[item] = prefix + item)
+        return constructor;
+    }
 }
 
 console.log(Greeter.greeting);
