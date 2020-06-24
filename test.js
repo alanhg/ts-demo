89188aface763428a81f7a706d7be927f6f4cf41
alanhe <qianghe421@gmail.com>
2020-06-24 16:38:58 +0800

    test class property load priority

--- test.js
+++ test.js
@@ -1,42 +1,12 @@
 "use strict";
-var __extends = (this && this.__extends) || (function () {
-    var extendStatics = function (d, b) {
-        extendStatics = Object.setPrototypeOf ||
-            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
-            function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
-        return extendStatics(d, b);
-    };
-    return function (d, b) {
-        extendStatics(d, b);
-        function __() { this.constructor = d; }
-        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
-    };
-})();
 exports.__esModule = true;
-var Greeter = /** @class */ (function () {
-    function Greeter() {
+var User = /** @class */ (function () {
+    function User() {
     }
-    Greeter.greeting = '';
-    Greeter.username = '';
-    return Greeter;
+    User.sayHello = function () {
+        console.log('hello2');
+    };
+    User.username = 'hello1';
+    return User;
 }());
-exports["default"] = Greeter;
-function modifyProp(target, propertyKey) {
-    target[propertyKey] = Math.random().toString();
-}
-function modifyProps(constructor) {
-    {
-        return /** @class */ (function (_super) {
-            __extends(class_1, _super);
-            function class_1() {
-                var _this = _super !== null && _super.apply(this, arguments) || this;
-                _this.greeting = Math.random().toString();
-                _this.username = Math.random().toString();
-                return _this;
-            }
-            return class_1;
-        }(constructor));
-    }
-}
-console.log(Greeter.greeting);
-console.log(Greeter.username);
+exports.User = User;
--- test.ts
+++ test.ts
@@ -1,21 +1,7 @@
-@modifyProps('2020')
-export default class Greeter {
-    static greeting = '';
+export class User {
+    static username = 'hello1';
 
-    static username = '';
-}
-
-function modifyProp(target: any, propertyKey: string) {
-    target[propertyKey] = Math.random().toString();
-}
-
-function modifyProps(prefix: string) {
-    return (constructor: any) => {
-        Object.keys(constructor).forEach(
-            item => constructor[item] = prefix + item)
-        return constructor;
+    static sayHello() {
+        console.log('hello2');
     }
 }
-
-console.log(Greeter.greeting);
-console.log(Greeter.username);
